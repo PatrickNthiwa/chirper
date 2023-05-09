a4015c08c347fcae99a66c1ecb48c918706bff46
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-09 18:59:41 +0300

    add a menu item for desktop screens"

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                        {{ __('Chirps') }}
+                    </x-nav-link>
                 </div>
             </div>
 
