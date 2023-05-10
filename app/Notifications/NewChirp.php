f137eff92da69cf2f2662e5c9c0d09b81aebf38c
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-10 13:52:31 +0300

    ";"

--- app/Notifications/NewChirp.php
+++ app/Notifications/NewChirp.php
@@ -3,7 +3,7 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use App\Model
+use App\Models\Chirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
